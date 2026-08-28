e3daf96f82722d440dc2da463afadde78b2cd9d5
Tom Deckers <tom@ducbase.com>
2026-08-28 11:20:50 +0200

    ci: test and package idle watch controller

--- tools/package.sh
+++ tools/package.sh
@@ -5,7 +5,7 @@ uuid=$(jq -r .uuid "$repo_dir/metadata.json")
 out_dir="$repo_dir/dist"
 rm -rf "$out_dir"
 mkdir -p "$out_dir/$uuid/schemas"
-cp "$repo_dir/metadata.json" "$repo_dir/extension.js" "$repo_dir/prefs.js" "$repo_dir/renderer.js" "$out_dir/$uuid/"
+cp "$repo_dir/metadata.json" "$repo_dir/extension.js" "$repo_dir/idleWatchController.js" "$repo_dir/prefs.js" "$repo_dir/renderer.js" "$out_dir/$uuid/"
 cp -R "$repo_dir/art" "$out_dir/$uuid/"
 cp "$repo_dir/schemas"/*.xml "$out_dir/$uuid/schemas/"
 (cd "$out_dir/$uuid" && zip -qr "../$uuid.zip" .)
